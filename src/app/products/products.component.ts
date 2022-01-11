f5b14b0ebf6cc723118b804870d83c672ad3b691
Sibi <sksibi123@gmail.com>
2022-01-11 23:50:51 +0530

    replaced multiple observables with switchMap Operator

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,9 +1,9 @@
+import { switchMap } from 'rxjs/operators';
 import { CategoryService } from './../services/category.service';
 import { ProductService } from 'src/app/services/product.service';
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../models/product';
 import { ActivatedRoute } from '@angular/router';
-
 
 @Component({
   selector: 'app-products',
@@ -16,18 +16,19 @@ filteredProducts:Product[]=[];
 categories:any[]=[];
 category:string='';
   constructor(private route: ActivatedRoute,productService:ProductService,categoryService:CategoryService) {
-  productService.getAll().subscribe((data)=>this.products=data);
+  productService.getAll().pipe(switchMap((data)=>{
+    this.products=data;
+    return this.route.queryParamMap;})).subscribe((params)=>{
+      this.category=params.get('category')!;
+      this.filteredProducts=this.category?this.products.filter((product)=>product.category==this.category):this.products;
+
+
+    });
   categoryService.getCategories().subscribe((data)=>this.categories=data);
 
-  this.route.queryParamMap.
 
 
-  subscribe((params)=>{
-    this.category=params.get('category')!;
-    this.filteredProducts=this.category?this.products.filter((product)=>product.category==this.category):this.products;
 
-
-  })
 
 
   }
