cbdf6c7031a5df5f5ada9479b3a2f81ab7888978
Sibi <sksibi123@gmail.com>
2022-01-11 23:25:26 +0530

    added products and product filtering feature

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -2,6 +2,7 @@ import { CategoryService } from './../services/category.service';
 import { ProductService } from 'src/app/services/product.service';
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../models/product';
+import { ActivatedRoute } from '@angular/router';
 
 
 @Component({
@@ -11,10 +12,24 @@ import { Product } from '../models/product';
 })
 export class ProductsComponent implements OnInit {
 products:Product[]=[];
+filteredProducts:Product[]=[];
 categories:any[]=[];
-  constructor(productService:ProductService,categoryService:CategoryService) {
+category:string='';
+  constructor(private route: ActivatedRoute,productService:ProductService,categoryService:CategoryService) {
   productService.getAll().subscribe((data)=>this.products=data);
   categoryService.getCategories().subscribe((data)=>this.categories=data);
+
+  this.route.queryParamMap.
+
+
+  subscribe((params)=>{
+    this.category=params.get('category')!;
+    this.filteredProducts=this.category?this.products.filter((product)=>product.category==this.category):this.products;
+
+
+  })
+
+
   }
 
   ngOnInit(): void {
